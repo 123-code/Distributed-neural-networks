4a9a5c3c47fcda3f070e9b3d798fb3c1f8b49d71
joseignacionaranjo <jose.naranjo.martinez@udla.edu.ec>
2025-04-09 18:07:17 -0500

    tensor sending logic

--- client.py
+++ client.py
@@ -5,23 +5,33 @@ import node_service_pb2_grpc
 import numpy as np
 
 async def run():
-    print("Starting client...")  # Debugging line
+    print("Starting client...")  
     try:
         async with grpc.aio.insecure_channel('192.168.1.202:50051') as channel:
-            print("Channel created...")  # Debugging line
+            print("Channel created...") 
             stub = node_service_pb2_grpc.NodeServiceStub(channel)
+            tensor_data = np.array([[1, 2], [3, 4]], dtype=np.float32)
+            shape = tensor_data.shape
+            tensor_data_bytes = tensor_data.tobytes()
+            request = node_service_pb2.TensorRequest(
+                request_id="tensor001",
+                tensor_data=tensor_data_bytes,
+                shape=shape,
+                dtype="float32"
+            )
             print("calling send message")
             message_request = node_service_pb2.MessageRequest(
                 sender_id = "client001",
                 message_text = "Hello from client",
             )
             try:
-                reply = await stub.SendMessage(message_request)
-                print(f"Received reply: {reply.confirmation_text}")
+        
+                reply = await stub.SendTensor(request)
+                print(f"Received reply: {reply.status}")
             except grpc.aio.AioRpcError as e:
                 print(f"RPC failed: {e.code()} - {e.details()}")
     except Exception as e:
-        print(f"An error occurred: {e}")  # Catch-all error
+        print(f"An error occurred: {e}") 
 
 if __name__ == '__main__':
     asyncio.run(run())
